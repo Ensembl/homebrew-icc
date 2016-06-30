50c7fe81f1de582a4cd4171bda9c0301e3872e14
Andrew Yates <andrewyatz@users.noreply.github.com>
2016-06-30 17:34:48 +0100

    Better error reporting

--- examl.rb
+++ examl.rb
@@ -2,12 +2,15 @@ class Examl < Formula
   homepage 'https://github.com/stamatak/ExaML'
   desc 'Exascale Maximum Likelihood (ExaML) code for phylogenetic inference using MPI'
   url 'https://github.com/stamatak/ExaML/archive/v3.0.17.tar.gz'
-  sha256 ''
+  sha256 '90a859e0b8fff697722352253e748f03c57b78ec5fbc1ae72f7e702d299dac67'
   version '3.0.17'
   
   def install
+    if `which mpicc` ~= /no mpicc/
+      odie 'Please make sure we have a mpicc available for compliation'
+    end
     if `mpicc` !~ /Intel/
-      odie 'Expect the Intel mpicc to be available for this'
+      odie 'Expect the Intel mpicc to be available for this buil'
     end
 
     cd 'examl' do
